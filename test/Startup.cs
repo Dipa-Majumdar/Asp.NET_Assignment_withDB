7fb6d40fdebf70256bcd97fa00646be61e853bee
Dipa Majumdar <DipaMajumdar@promact.com>
2018-03-06 12:01:09 +0530

    completed

--- test/Startup.cs
+++ test/Startup.cs
@@ -21,9 +21,6 @@ namespace test
             _configuration = configuration;
         }
 
-        
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ProductInventoryDbContext>(options => options.UseSqlServer(_configuration.GetConnectionString("ProductInventory")));
@@ -31,7 +28,6 @@ namespace test
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
